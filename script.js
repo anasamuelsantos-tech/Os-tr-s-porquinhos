a5069651f93edcd9a9d1620fce1dc4c05f7dd21f
anasamuelsantos-tech <ana.samuel.santos@escola.pr.gov.br>
2025-10-22 08:41:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const avance=document.querySelectorAll('.btn-proximo'
+
+avanca.forEach( button=>{
+    button.addEventlistener('clik', function)(){
+        const atual=document.querySelector('ativo')
+        const proximopasso=('psso-'+this.getAttribute(data-proximo)
+        atual.classList.remove('ativo')
+        document.getElementById(proximopasso).classlist()
+
+    }
+}
+    
+);
